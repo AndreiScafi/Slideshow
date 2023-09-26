4aea693e8904f1bda7d158a366d519b9c8f544f3
Andrei Scafi <andreiscafi@hotmail.com>
2023-09-26 11:05:02 -0300

    Making slides change automtically with Javascript.

---
+++ script.js
@@ -0,0 +1,29 @@
+let current = 1;
+
+const changeSlides = () => {
+    const slideList = document.querySelectorAll('.slide');
+
+    const slides = Array.from(slideList);
+
+    if (current > slides.length) {
+        current = 1;
+    }
+
+    slides.forEach(slide => {
+        if (slide.classList[1].split('-')[1] * 1 === current) {
+            slide.style.cssText = 'visibility: visible; opacity: 1'
+        } else {
+            slide.style.cssText = 'visibility: hidden; opacity: 0'
+        }
+    })
+};
+
+const playPause = () => {
+    setInterval(() => {
+        current++;
+        changeSlides();
+    }, 3000)
+}
+
+changeSlides();
+playPause(); 
